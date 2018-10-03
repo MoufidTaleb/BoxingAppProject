5834ccc244c019e95ec6bfe88b7b5e14b3db291b
Moufid <moufid.taleb@gmail.com>
2018-10-04 01:23:02 +0200

    Created the Advert entity, and implemented add and delete features using the form builder

--- src/TI/PlatformBundle/Controller/AdvertController.php
+++ src/TI/PlatformBundle/Controller/AdvertController.php
@@ -4,22 +4,53 @@ namespace TI\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use TI\PlatformBundle\Entity\Advert;
+use TI\PlatformBundle\Form\AdvertType;
 
 class AdvertController extends Controller
 {
     public function indexAction($page)
     {
-        return $this->render('TIPlatformBundle:Advert:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $listAdverts = $em->getRepository('TIPlatformBundle:Advert')->findAll();
+
+        return $this->render('TIPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts,
+        ));
     }
 
     public function viewAction($id)
     {
-        return $this->render('TIPlatformBundle:Advert:view.html.twig');
+        $repository = $this->getDoctrine()->getManager()->getRepository('TIPlatformBundle:Advert');
+        $advert = $repository->find($id);
+
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce n'existe pas!");
+        }
+        return $this->render('TIPlatformBundle:Advert:view.html.twig', array(
+            'advert' => $advert,
+        ));
     }
 
     public function addAction(Request $request)
     {
-        return $this->render('TIPlatformBundle:Advert:add.html.twig');
+        $advert = new Advert();
+        $form = $this->createForm(AdvertType::class, $advert);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($advert);
+            $em->flush();
+
+            return $this->redirectToRoute('ti_platform_view', array(
+                'id' => $advert->getId(),
+            ));
+        }
+        return $this->render('TIPlatformBundle:Advert:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     public function editAction(Request $request, $id)
@@ -29,11 +60,32 @@ class AdvertController extends Controller
 
     public function deleteAction(Request $request, $id)
     {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('TIPlatformBundle:Advert');
+        $advert = $repository->find($id);
+
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id " .$id. " n'existe pas!");
+        }
+
+        $em->remove($advert);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('info', "L'annonce a bien été supprimée!");
         return $this->render('TIPlatformBundle:Advert:delete.html.twig');
     }
 
     public function menuAction()
     {
-        return $this->render('TIPlatformBundle:Advert:menu.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $listAdverts = $em->getRepository('TIPlatformBundle:Advert')->findBy(
+            array(),
+            array('date' => 'desc'),
+            3,
+            0
+        );
+        return $this->render('TIPlatformBundle:Advert:menu.html.twig', array(
+            'listAdverts' => $listAdverts,
+        ));
     }
 }
---
+++ src/TI/PlatformBundle/Entity/Advert.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace TI\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Advert
+ *
+ * @ORM\Table(name="advert")
+ * @ORM\Entity(repositoryClass="TI\PlatformBundle\Repository\AdvertRepository")
+ */
+class Advert
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetimetz")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="location", type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Advert
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Advert
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Advert
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Advert
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Advert
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set location
+     *
+     * @param string $location
+     *
+     * @return Advert
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Get location
+     *
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+}
